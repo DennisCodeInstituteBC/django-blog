a133cfc99ddde044eac2f84e56d2884a7cbdb406
Dennis Limbu <87oyrsom@students.codeinstitute.net>
2024-09-14 14:33:58 +0000

    adding 'Procfile' to be able to host in Heroku

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a9qfcf(x!i^sd!qfs@pf)^xoa2d7d!gdc6#7sclr!4fctuf@l0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-denniscodein-djangoblog-hekijbdmwfv.ws.codeinstitute-ide.net']
 
