8fd94aadfa21e6b49ab8c99e704f0e9d2233be53
Dennis Limbu <87oyrsom@students.codeinstitute.net>
2024-09-15 21:58:38 +0000

    adding Databases and deployment

--- env.py
+++ env.py
@@ -2,3 +2,5 @@ import os
 
 os.environ.setdefault(
     "DATABASE_URL", "postgres://uinr77ef9q4:SkZpxdX9B6qd@ep-gentle-mountain-a23bxz6h-pooler.eu-central-1.aws.neon.tech/void_growl_drill_907282")
+os.environ.setdefault(
+    "SECRET_KEY", "(j(j_+@%s$a955-5gw=m@b-%#$slmv$0aixrck&odnq*h+ig@0")
